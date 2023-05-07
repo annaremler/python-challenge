4d5e3bac5e4c58e9ebfd17dd3220b76e85789c0c
Anna Remler <84870433+annaremler@users.noreply.github.com>
2023-05-06 17:56:00 -0700

    Reupload
    Second submission

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,73 +4,57 @@ import csv
 
 # Part 1: PyBank
 ## Pull CSV file "budget_data.csv" and set path
-pybank_csv = os.path.join ("Resources", "budget_data.csv")
-
-## Open CSV as reader
-with open(pybank_csv, newline="", encoding='utf-8') as csvfile:
+pybank_csv = os.path.join("Resources", "budget_data.csv")
+with open(pybank_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
 
-    ## Create lists
+## Create variables and lists
     profit = []
     difference = []
-    date = []
+    dates = []
 
-    ## Insert variables
     count = 0
     total_profit = 0
     revenue_change = 0
     initial_profit = 0
 
-    ## Coding the chart
-    for row in csvreader:    
-        # Use count to count the number months in this dataset
-        count += 1 
-
-        # Append onto "date"
-        date.append(row[0])
-
-        # Append profit and calculate total profit
+    for row in csvreader:
+        count = count + 1
+        dates.append(row[0])
         profit.append(row[1])
         total_profit = total_profit + int(row[1])
 
-        #Calculate average change in profits monthly
-        final_profit = int(row[1])
-        difference_profits = final_profit - initial_profit
+        difference_profit = int(row[1])
+        monthly_difference = difference_profit - initial_profit
+        difference.append(monthly_difference)
 
-        #Store monthly changes in a list
-        difference.append(difference_profits)
+        revenue_change = revenue_change + monthly_difference
+        initial_profit = difference_profit
+        average_profits = monthly_difference / count
 
-        revenue_change = revenue_change + difference_profits
-        initial_profit = final_profit
+        greatest_increase = max(difference)
+        increase_month = dates[difference.index(greatest_increase)]
+        greatest_decrease = min(difference)
+        decrease_month = dates[difference.index(greatest_decrease)]
 
-        ##Calculate average change in profits
-        average_revenue_change = (revenue_change/count)
-        
-        ##Find max and min change in profits + the dates that changes occured
-        greatest_increase_profits = max(difference)
-        greatest_decrease_profits = min(difference)
 
-        increase_date = date[difference.index(greatest_increase_profits)]
-        decrease_date = date[difference.index(greatest_decrease_profits)]
+## Check that data Financial Analysis checks out
+    print(f'PyBank Analysis')
 
-    ## Check that data Financial Analysis checks out
-    print(f"Financial Analysis")
-    print(f"----------------------------------------------------------")
-    print(f"Total Months: " + str(count))
-    print(f"Total Profits: " + "$" + str(total_profit))
-    print(f"Average Change: " + "$" + str(int(average_revenue_change)))
-    print(f"Greatest Increase in Profits: " + str(increase_date) + " ($" + str(greatest_increase_profits) + ")")
-    print(f"Greatest Decrease in Profits: " + str(decrease_date) + " ($" + str(greatest_decrease_profits)+ ")")
-    
-    ##Open into text file
-with open('financial_analysis.txt', 'w') as text:
-    text.write("----------------------------------------------------------\n")
-    text.write("  Financial Analysis"+ "\n")
-    text.write("----------------------------------------------------------\n\n")
-    text.write("    Total Months: " + str(count) + "\n")
-    text.write("    Total Profits: " + "$" + str(totalProfit) +"\n")
-    text.write("    Average Change: " + '$' + str(int(averagechangeProfits)) + "\n")
-    text.write("    Greatest Increase in Profits: " + str(increase_date) + " ($" + str(grtstincreaseProfits) + ")\n")
-    text.write("    Greatest Decrease in Profits: " + str(decrease_date) + " ($" + str(grtstdecreaseProfits) + ")\n")
-    text.write("----------------------------------------------------------\n")
+    print(f'Total Months: {len(dates)}')
+    print(f'Total: ' + "$" + str(total_profit))
+    print(f'Average Change: ' + "$" + str(int(average_profits)))
+    print(f'Greatest Increase in Profits: ' + str(increase_month) + " ($" + str(greatest_increase) + ")")
+    print(f'Greatest Decrease in Profits: ' + str(decrease_month) + " ($" + str(greatest_decrease) + ")")
+
+
+## Open into text file
+    with open("Financial_analysis.txt", "w") as text:
+        text.write(f'PyBank Analysis' + "\n")
+
+        text.write(f'Total Months: {len(dates)}' + "\n")
+        text.write(f'Total: ' + "$" + str(total_profit) + "\n")
+        text.write(f'Average Change: ' + "$" + str(int(average_profits)) + "\n")
+        text.write(f'Greatest Increase in Profits: ' + str(increase_month) + " ($" + str(greatest_increase) + ")\n")
+        text.write(f'Greatest Decrease in Profits: ' + str(decrease_month) + " ($" + str(greatest_decrease) + ")\n")
